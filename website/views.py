de9665341786afeaf58b5fea18654b81e8818669
Jacob Wejendorp <jacob@wejendorp.dk>
2012-06-26 17:27:06 +0200

    Easteregg.. left as an exercise to the reader

--- website/views.py
+++ website/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def index(request):
+    return render_to_response('websits/index.html')
+
+def easteregg(request):
+    # TODO
+    pass
