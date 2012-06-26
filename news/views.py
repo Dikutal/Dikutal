60b289cecdd492aad97cc9c47b27101babfc4528
Daniel Egeberg <daniel.egeberg@gmail.com>
2012-06-26 16:49:04 +0200

    Article vs. NewsArticle.

--- news/views.py
+++ news/views.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from news.models import Article
+from news.models import Article, NewsArticle
 
-def index(request):
+def article_index(request):
     latest = Article.objects.all().order_by('-published')
     return render_to_response('news/index.html', {'latest': latest})
 
-def view(request, id, slug):
+def article_view(request, id, slug):
     article = get_object_or_404(Article, pk=id)
+    return render_to_response('news/view.html', RequestContext(request, {'article': article}))
+
+def news_index(request):
+    latest = NewsArticle.objects.all().order_by('-published')
+    return render_to_response('news/index.html', {'latest': latest})
+
+def news_view(request, id, slug):
+    article = get_object_or_404(NewsArticle, pk=id)
     return render_to_response('news/view.html', RequestContext(request, {'article': article}))
