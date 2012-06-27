4dcbc505def5b39c3771a27caf2de59c9423f46c
Daniel Egeberg <daniel.egeberg@gmail.com>
2012-06-27 10:17:52 +0200

    Article pagination.

--- news/views.py
+++ news/views.py
@@ -1,20 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from news.models import Article, NewsArticle
 
-def article_index(request):
-    latest = Article.objects.all().order_by('-published')
+import datetime
+
+def index(model, request):
+    articles = model.objects.filter(published__lt=datetime.datetime.now()).order_by('-published')
+    paginator = Paginator(articles, 10)
+
+    page = request.GET.get('page')
+    try:
+        latest = paginator.page(page)
+    except PageNotAnInteger:
+        latest = paginator.page(1)
+    except EmptyPage:
+        latest = paginator.page(paginator.num_pages)
+
     return render_to_response('news/index.html', {'latest': latest})
 
-def article_view(request, id, slug):
-    article = get_object_or_404(Article, pk=id)
+def view(model, request, id, slug):
+    article = get_object_or_404(model, pk=id)
     return render_to_response('news/view.html', RequestContext(request, {'article': article}))
+
+def article_index(request):
+    return index(Article, request)
 
 def news_index(request):
-    latest = NewsArticle.objects.all().order_by('-published')
-    return render_to_response('news/index.html', {'latest': latest})
+    return index(NewsArticle, request)
+
+def article_view(request, id, slug):
+    return view(Article, request, id, slug)
 
 def news_view(request, id, slug):
-    article = get_object_or_404(NewsArticle, pk=id)
-    return render_to_response('news/view.html', RequestContext(request, {'article': article}))
+    return view(NewsArticle, request, id, slug)
