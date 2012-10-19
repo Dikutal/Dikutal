f6a3ad60bd8b4b6e20e3d7322a07d65cbacc38f8
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2012-10-19 16:45:47 +0200

    Add owner and title to planet feeds

--- planet/models.py
+++ planet/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 class PlanetFeed(models.Model):
+    title = models.CharField(max_length=300)
     url = models.URLField()
+    owner = models.ForeignKey(User, related_name='%(class)ss', null=True)
     last_edited = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -12,7 +15,8 @@ class PlanetFeed(models.Model):
 class PlanetFeedAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ('url',)
+            'fields': ('title', 'url', 'owner')
         }),
         )
+    list_display = ['title', 'url', 'owner']
 
