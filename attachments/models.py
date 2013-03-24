fb5caeafe10245cc106ae25b78b157c41a47daed
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2013-03-24 12:28:08 +0100

    Don't require directory name dikutal

--- attachments/models.py
+++ attachments/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
-from dikutal.settings import *
+from settings import *
 
 from datetime import datetime
 
